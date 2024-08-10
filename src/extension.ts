81fd4786551787358a291d81bef4b24bae02fa75
kazzarahw <kazzarahwoods@protonmail.com>
2024-08-09 20:26:28 -0500

    0.0.4

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,19 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('copy-filepath-and-line-number.run', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const filePath = editor.document.uri.fsPath;
-            const lineNumber = editor.selection.active.line + 1; // Line numbers in VS Code are 0-based
-
-            const formattedString = `${filePath}#L${lineNumber}`;
-            vscode.env.clipboard.writeText(formattedString).then(() => {
-                vscode.window.showInformationMessage('Copied to clipboard: ' + formattedString);
-            });
-        } else {
-            vscode.window.showInformationMessage('No active editor found');
-        }
-    });
-
-    context.subscriptions.push(disposable);
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const filePath = vscode.workspace.asRelativePath(editor.document.fileName);
+			const lineNumber = editor.selection.active.line + 1;
+			const formattedString = `${filePath}#L${lineNumber}`;
+			vscode.env.clipboard.writeText(formattedString).then(() => {
+				vscode.window.showInformationMessage('Copied to clipboard: ' + formattedString);
+			});
+		} else {
+			vscode.window.showErrorMessage('No active text editor');
+		}
+	});
+	context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() { }
