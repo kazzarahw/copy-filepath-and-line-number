dfb89d312471da6b6288a0c89b58f8db97fb7c98
kazzarahw <kazzarahwoods@protonmail.com>
2024-08-09 19:46:23 -0500

    0.0.2

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,33 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('copy-filepath-and-line-number.run', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const filePath = editor.document.uri.fsPath;
-            const lineNumber = editor.selection.active.line + 1; // Line numbers in VS Code are 0-based
+	let disposable = vscode.commands.registerCommand('extension.copyFilePathAndLineNumber', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			const filePath = document.uri.fsPath;
+			const lineNumber = editor.selection.active.line + 1;
 
-            const formattedString = `${filePath}#L${lineNumber}`;
-            vscode.env.clipboard.writeText(formattedString).then(() => {
-                vscode.window.showInformationMessage('Copied to clipboard: ' + formattedString);
-            });
-        } else {
-            vscode.window.showInformationMessage('No active editor found');
-        }
-    });
+			// Get the workspace folder
+			const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
+			let relativeFilePath = filePath;
 
-    context.subscriptions.push(disposable);
+			if (workspaceFolder) {
+				// Convert to relative path
+				relativeFilePath = path.relative(workspaceFolder.uri.fsPath, filePath);
+			}
+
+			const formattedString = `${relativeFilePath}#L${lineNumber}`;
+			vscode.env.clipboard.writeText(formattedString).then(() => {
+				vscode.window.showInformationMessage('Copied to clipboard: ' + formattedString);
+			});
+		} else {
+			vscode.window.showInformationMessage('No active editor found');
+		}
+	});
+
+	context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() { }
